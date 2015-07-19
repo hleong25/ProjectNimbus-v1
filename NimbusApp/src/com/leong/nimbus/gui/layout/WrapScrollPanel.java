e7d520ded1d636bf87670cb081390c2940c672e2
Henry Leong <hleong25@gmail.com>
2015-07-19 16:34:44 -0700

    Rename variables

--- NimbusApp/src/com/leong/nimbus/gui/layout/WrapScrollPanel.java
+++ NimbusApp/src/com/leong/nimbus/gui/layout/WrapScrollPanel.java
@@ -44,64 +44,67 @@ public class WrapScrollPanel extends javax.swing.JPanel
     {
 
         pnlScroll = new javax.swing.JScrollPane();
-        pnlFiles = new javax.swing.JPanel();
+        pnlComps = new javax.swing.JPanel();
 
         setLayout(new java.awt.BorderLayout());
 
         pnlScroll.setMinimumSize(new java.awt.Dimension(400, 300));
         pnlScroll.setPreferredSize(new java.awt.Dimension(400, 300));
 
-        pnlFiles.setBackground(new java.awt.Color(255, 255, 255));
+        pnlComps.setBackground(new java.awt.Color(255, 255, 255));
         WrapLayout wraplayout = new WrapLayout(java.awt.FlowLayout.LEADING);
         wraplayout.setAlignOnBaseline(true);
-        pnlFiles.setLayout(wraplayout);
-        pnlFiles.addMouseListener(new java.awt.event.MouseAdapter()
+        pnlComps.setLayout(wraplayout);
+        pnlComps.addMouseListener(new java.awt.event.MouseAdapter()
         {
             public void mouseClicked(java.awt.event.MouseEvent evt)
             {
-                pnlFilesMouseClicked(evt);
+                pnlCompsMouseClicked(evt);
             }
         });
-        pnlFiles.addKeyListener(new java.awt.event.KeyAdapter()
+        pnlComps.addKeyListener(new java.awt.event.KeyAdapter()
         {
             public void keyReleased(java.awt.event.KeyEvent evt)
             {
-                pnlFilesKeyReleased(evt);
+                pnlCompsKeyReleased(evt);
             }
         });
-        pnlScroll.setViewportView(pnlFiles);
+        pnlScroll.setViewportView(pnlComps);
 
         add(pnlScroll, java.awt.BorderLayout.CENTER);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void pnlFilesMouseClicked(java.awt.event.MouseEvent evt)//GEN-FIRST:event_pnlFilesMouseClicked
-    {//GEN-HEADEREND:event_pnlFilesMouseClicked
-        pnlFiles.requestFocusInWindow();
-    }//GEN-LAST:event_pnlFilesMouseClicked
+    private void pnlCompsMouseClicked(java.awt.event.MouseEvent evt)//GEN-FIRST:event_pnlCompsMouseClicked
+    {//GEN-HEADEREND:event_pnlCompsMouseClicked
+        pnlComps.requestFocusInWindow();
+    }//GEN-LAST:event_pnlCompsMouseClicked
 
-    private void pnlFilesKeyReleased(java.awt.event.KeyEvent evt)//GEN-FIRST:event_pnlFilesKeyReleased
-    {//GEN-HEADEREND:event_pnlFilesKeyReleased
-        Log.entering("pnlFilesKeyReleased", evt);
-        m_proxy.proxyKeyReleased(evt);
-    }//GEN-LAST:event_pnlFilesKeyReleased
+    private void pnlCompsKeyReleased(java.awt.event.KeyEvent evt)//GEN-FIRST:event_pnlCompsKeyReleased
+    {//GEN-HEADEREND:event_pnlCompsKeyReleased
+        Log.entering("pnlCompsKeyReleased", evt);
+        if (m_proxy != null)
+        {
+            m_proxy.proxyKeyReleased(evt);
+        }
+    }//GEN-LAST:event_pnlCompsKeyReleased
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel pnlFiles;
+    private javax.swing.JPanel pnlComps;
     private javax.swing.JScrollPane pnlScroll;
     // End of variables declaration//GEN-END:variables
 
     @Override
     public void removeAll()
     {
-        pnlFiles.removeAll();
+        pnlComps.removeAll();
     }
 
     @Override
     public Component add(Component comp)
     {
         //Log.entering("add", comp);
-        return pnlFiles.add(comp);
+        return pnlComps.add(comp);
     }
 
 }
