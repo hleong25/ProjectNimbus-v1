ad7e3b98b4e857c29450e369268161bbed03090d
Henry Leong <hleong25@gmail.com>
2015-08-08 15:44:39 -0700

    Add OSX app setup

--- NimbusApp/src/com/leong/nimbus/mainapp/MainGuiApp.java
+++ NimbusApp/src/com/leong/nimbus/mainapp/MainGuiApp.java
@@ -21,6 +21,8 @@ public class MainGuiApp
         Logit.init();
         Log.entering("main");
 
+        setupPlatform();
+
         //setupLookAndFeel();
 
         //PickCloudFrame.showMe();
@@ -33,7 +35,7 @@ public class MainGuiApp
         {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
             {
-                if ("Nimbus".equals(info.getName()))
+                if (AppInfo.Name.equals(info.getName()))
                 {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
@@ -45,6 +47,27 @@ public class MainGuiApp
             Log.throwing("setupLookAndFeel", ex);
         }
 
+    }
+
+    private static void setupPlatform()
+    {
+        // http://stackoverflow.com/questions/22604218/set-a-dynamic-apple-menu-title-for-java-program-in-netbeans-7-4
+        // http://stackoverflow.com/questions/3154638/setting-java-swing-application-name-on-mac
+
+        // the application menu for Mac OS X must be set very early in the cycle
+        String osName = System.getProperty("os.name").toLowerCase();
+
+        if (osName.contains("mac"))
+        {
+            // to set the name of the app in the Mac App menu:
+            System.setProperty("apple.awt.application.name", AppInfo.Name);
+
+            //to show the menu bar at the top of the screen:
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+
+            // to show a more mac-like file dialog box
+            //System.setProperty("apple.awt.fileDialogForDirectories", "true");
+        }
     }
 
 }
